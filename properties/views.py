39eaf91aeb2e2addab82ae33918f1d2b04e4ad93
cox101 <104098884+cox101@users.noreply.github.com>
2025-08-13 22:05:01 +0300

    update

--- properties/views.py
+++ properties/views.py
@@ -19,3 +19,11 @@ from .models import Property
 def property_list(request):
     properties = list(Property.objects.values())
     return JsonResponse({"properties": properties})
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
+
+@cache_page(60 * 15)  # Cache for 15 minutes
+def property_list(request):
+    properties = list(Property.objects.values())
+    return JsonResponse({"properties": properties})
